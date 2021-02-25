2998bebc18656cb3a143e6b0475f206538fed585
Aaron Hernandez <aah007@ucsd.edu>
2021-02-25 10:33:03 -0800

    editing json reading

--- routes/all_courses.js
+++ routes/all_courses.js
@@ -4,11 +4,32 @@
 var data = require("../data.json");
 
 exports.view = function(request, response){
+  var courses = [];
+  var courseList = [];
+
+  for (key in data){
+    courses.push(key);
+  }
+
+  for(var i = 0; i < courses.length; i++){
+    var newCourse = {
+      "class": data[courses[i]][0].class,
+      "professer": data[courses[i]][0].professer,
+			"grade": data[courses[i]][0].grade
+    };
+
+    courseList.push(newCourse);
+  }
+
+  var tempDict = {
+    classes: courseList
+  };
+
   var username = request.body.username;
 	var password = request.body.password;
 
-  data["username"] = username;
-  data["password"] = password;
+  tempDict["username"] = username;
+  tempDict["password"] = password;
 
-  response.render('all_courses', data);
+  response.render('all_courses', tempDict);
 };
